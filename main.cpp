4e2a8fbe62177c99ca3afd3641568288588440fe
stanislavkuskov <kuskov.stanislav@gmail.com>
2020-01-09 09:51:53 +0300

    First task

--- main.cpp
+++ main.cpp
@@ -11,13 +11,23 @@ using matrix_storage = std::vector<std::tuple<int, int, int>> ;
 template <typename T, T default_value>
 class Matrix
 {
-public:
+private:
     struct MatrixContainer {
         matrix_storage m_container;
         void append_element(int h, int w, int val){
-            if (find_element(h, w) == -1){
-                m_container.emplace_back(h, w, val);
+//          Проверяем val на значение по умолчанию
+            if (val != default_value){
+                if (find_element(h, w) == default_value){
+                    m_container.emplace_back(h, w, val);
+                }
+            } else {
+                if (find_element(h, w) != default_value){
+//                    TODO найти элемент и удалить его из контейнера (наверное нужно заменить вектор на что то другое)
+
+                    m_container.emplace_back(h, w, val);
+                }
             }
+
         };
         int find_element(int h, int w){
             for (std::tuple<int, int, int> element : m_container){
@@ -51,16 +61,47 @@ public:
 
 int main(int, char *[])
 {
-    Matrix<int, -1> matrix;
-    assert(matrix.size() == 0);
+//    Matrix<int, -1> matrix;
+//    assert(matrix.size() == 0);
+//
+//    auto a = matrix.get_element(0, 0);
+//    assert(a == -1);
+//    assert(matrix.size() == 0);
+//
+//    matrix.set_element(100, 100, 314);
+//    assert(matrix.get_element(100, 100) == 314);
+//    assert(matrix.size() == 1);
 
-    auto a = matrix.get_element(0, 0);
-    assert(a == -1);
-    assert(matrix.size() == 0);
+    int const default_value = 0;
+    Matrix<int, default_value> m;
 
-    matrix.set_element(100, 100, 314);
-    assert(matrix.get_element(100, 100) == 314);
-    assert(matrix.size() == 1);
+    /*  1. создать матрицу с пустым значением 0, заполнить диагонали матрицы
+     * выражением m[i][i] = m[N - 1 - i][i] = i; // N = 10, i = (0 .. N)
+    */
 
+//    DONE
+    int N = 10;
+
+    for (int i = 0; i < N; ++i){
+        m.set_element(i, i, i);
+        m.set_element(N - 1 - i, i, i);
+    }
+
+    /*  2. Начиная с ячейки [0, 0] в шахматном порядке заполнить матрицу 10x10
+     * значением по умолчанию.
+     */
+//        ПРОВЕРКА НА ЗНАЧЕНИЯ ПО УМОЛЧАНИЮ
+//    for (int i = 0; i <= N; ++i){
+//        m.set_element(i, i, default_value);
+//    }
+
+    /*  3. Необходимо вывести фрагмент матрицы от [1,1] до [8,8]. Между столбцами пробел.
+     * Каждая строка матрицы на новой строке.
+     */
+
+    /*  4. Вывести количество занятых ячеек.*/
+    std::cout << m.size() << std::endl;
+
+    /*  5. Вывести все занятые ячейки вместе со своими позициями.*/
     return 0;
 }
