c02095e809dad0a706d764f280a3c3f8bd2e4fd7
stanislavkuskov <kuskov.stanislav@gmail.com>
2020-01-13 00:38:14 +0300

    Add print of matrix 8x8 and all filled cells with positions

--- main.cpp
+++ main.cpp
@@ -3,9 +3,6 @@
 #include <vector>
 #include <map>
 
-#include <iostream>
-#include <map>
-
 using matrix_storage = std::map<std::pair<int, int>, int> ;
 
 template <typename T, T default_value>
@@ -14,7 +11,7 @@ class Matrix
 private:
     struct MatrixContainer {
         matrix_storage m_container;
-        void append_element(int h, int w, int val){
+        void append_element(int h, int w, T val){
 //          Проверяем val на значение по умолчанию
             if (val != default_value){
                 m_container.insert({{h, w}, val});
@@ -37,6 +34,15 @@ private:
         size_t get_container_size(){
             return m_container.size();
         }
+        void print_container(){
+            for (auto element: m_container){
+                std::cout << "value: " << element.second
+                << ", h: " << element.first.first << ", w: "
+                << element.first.second << std::endl;
+            }
+
+        }
+
     };
 
     MatrixContainer m;
@@ -51,8 +57,11 @@ public:
         return m.get_value(h, w);
     }
 
-    void set_element(int h, int w, int val){
+    void set_element(int h, int w, T val){
         m.append_element(h, w, val);
+    }
+    void print(){
+        m.print_container();
     }
 };
 
@@ -105,17 +114,20 @@ int main(int, char *[])
      * Каждая строка матрицы на новой строке.
      */
 // вывод элементов по списку, отображение 0 на пустых местах. Для этго нужен упорядоченный список
-    for (int i = 1; i <= 8; ++i){
-        for (int j = 1; j <= 8; ++j){
-            std::cout << m.get_element(i, j) << " ";
-
+    for (int h = 1; h <= 8; ++h){
+        for (int w = 1; w <= 8; ++w){
+            std::cout << m.get_element(h, w) << " ";
         }
         std::cout << std::endl;
     }
+    std::cout << std::endl;
 
     /*  4. Вывести количество занятых ячеек.*/
     std::cout << m.size() << std::endl;
-
+    std::cout << std::endl;
     /*  5. Вывести все занятые ячейки вместе со своими позициями.*/
+    m.print();
+    std::cout << std::endl;
+
     return 0;
 }
